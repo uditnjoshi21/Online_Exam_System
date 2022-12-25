0cbf7dadb1dc9c658ba8ac23129fc287f967880c
uditnjoshi21 <uditnjoshi21@gmail.com>
2022-12-25 14:27:00 -0500

    update the reset method

--- 5.student_homepage.php
+++ 5.student_homepage.php
@@ -22,6 +22,7 @@ session_start();
         <form method=post action=5.student_homepage.php>
             <p align="center"> <input type="submit" value="Profile" name="profile"> </p>
         </form>
+        <!-- Chnage the reset password method -->
         <form method=post action=2.reset_password.php>
             <p align="center"> <input type="submit" value="Update Password" name="update_passward"> </p>
         </form>
